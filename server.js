519beeae4c09fe7a0e69f25e19af8163b3bc2f30
Bjarke Mulbjerg <bjarkemulbjerg12@gmail.com>
2017-05-08 15:58:05 +0000

    fixes

--- server.js
+++ server.js
@@ -5,18 +5,27 @@ var app = express()
 app.get('/:query', function (req, res) {
     var query = req.params.query;
     var date;
+    
     if(/[a-z]/.test(query)) { 
         date = new Date(query)
         
-    } else {
+    } else if(!isNaN(query)) {
         date = new Date(+query);
     }
+    if(date) {
+        res.send({
+            unix: date.getTime(),
+            normal: `${utils.getMonthName(date.getMonth())} ${date.getDate()}, ${date.getFullYear()}`,
+        });    
+    } else {
+        res.send({
+            unix: null,
+            normal: null
+        });
+    }
     
-    res.send({
-        unix: date.getTime(),
-        normal: `${utils.getMonthName(date.getMonth())} ${date.getDate()}, ${date.getFullYear()}`,
-    });
-})
+});
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
