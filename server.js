ee5b3ea42f30898ece6fa974c729f13c629837fe
Bjarke Mulbjerg <bjarkemulbjerg12@gmail.com>
2017-05-08 15:51:05 +0000

    timestamp micro service

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var express = require('express')
+var utils = require("./utils");
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:query', function (req, res) {
+    var query = req.params.query;
+    var date;
+    if(/[a-z]/.test(query)) { 
+        date = new Date(query)
+        
+    } else {
+        date = new Date(+query);
+    }
+    
+    res.send({
+        unix: date.getTime(),
+        normal: `${utils.getMonthName(date.getMonth())} ${date.getDate()}, ${date.getFullYear()}`,
+    });
 })
 
 app.listen(8080, function () {
---
+++ utils.js
@@ -0,0 +1,8 @@
+const getMonthName = (monthNumber) => {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    return months[monthNumber];
+}
+
+module.exports = {
+    getMonthName: getMonthName
+}
